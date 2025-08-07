bb0ead14cced45cc5ec946e5d7f85cf6ce55c9d9
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2025-08-07 14:22:13 +0530

    Initial project upload

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db")
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        method: ["GET","POST","PUT","DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+//Connect Database
+connectDB();
+
+//Middleware
+app.use(express.json());
+
+
+
+//Routes
+
+
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
